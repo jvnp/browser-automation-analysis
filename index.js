5f3264a34c02c0950096c11436c36d52fe2a22ef
Jeevan Pant <jeevanppant@gmail.com>
2022-05-14 17:38:07 +0530

    fix: lint with semicolon for index

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+/* eslint semi: "error" */
 const chalk = require('chalk');
 const timetest = require('./src/timetest');
 
 // Time Test for HTML extraction
 console.log(chalk.red(' Time Test HTML \n -------------- '));
-( async () => {
-    const execTime = await timetest.timeTestHtml()
-    console.log(chalk.blue(` Puppeteer  : ${execTime.puppeteerExecTime} seconds`))
-    console.log(chalk.blue(` Playwright : ${execTime.playwrightExecTime} seconds \n`))
+(async () => {
+  const execTime = await timetest.timeTestHtml();
+  console.log(chalk.blue(` Puppeteer  : ${execTime.puppeteerExecTime} seconds`));
+  console.log(chalk.blue(` Playwright : ${execTime.playwrightExecTime} seconds \n`));
 })().catch(err => {
-    console.error(err)
-})
-
-
-
+  console.error(err);
+});
