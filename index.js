1a60c6da767a47601c08dba1af649c64057897a8
Jeevan Pant <jeevanppant@gmail.com>
2022-05-21 01:29:13 +0530

    update: filename to camelCase

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /* eslint semi: "error" */
 const chalk = require('chalk');
-const timetest = require('./src/timetest');
+const timetest = require('./src/timeTest');
 
-const config = require('./configs/app.json')
-const url = (process.argv.length === 3) ? process.argv[2] : config.url
+const config = require('./configs/app.json');
+const url = (process.argv.length === 3) ? process.argv[2] : config.url;
 
 console.log(chalk.red(` URL: ${url} `));
 
 // Time Test for HTML extraction
 console.log(chalk.red(' Time Test HTML \n -------------- '));
 (async () => {
-
   const execTime = await timetest.timeTestHtml(url);
 
   console.log(chalk.blue(` Puppeteer  : ${execTime.puppeteerExecTime} seconds`));
--- test/timetest.test.js
+++ test/timetest.test.js
@@ -1,4 +1,4 @@
-const timetest = require('../src/timetest')
+const timetest = require('../src/timeTest')
 
 test('time test', async () => {
   const execTime = await timetest.timeTestHtml('https://bolster.ai')
